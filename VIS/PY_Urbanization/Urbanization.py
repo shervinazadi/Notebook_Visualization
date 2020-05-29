693109c1dc1fa430fefa9e1a42cee8ffd56790a3
Shervin Azadi <shervinazadi.arch@gmail.com>
2020-05-29 23:49:32 +0200

    remove before 1950

--- VIS/PY_Urbanization/Urbanization.py
+++ VIS/PY_Urbanization/Urbanization.py
@@ -1,13 +1,23 @@
 import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
 
 # loading the data
 data_path = "DATA/urban-vs-rural-majority.csv"
-df = pd.read_csv(data_path, index_col=1)
+df = pd.read_csv(data_path)
+
+# remove everything before 1950
+df = df[df['Year'] >= 1950]
 
 # group by countries
 grouped_df = df.groupby(['Entity'])
 
-for g in grouped_df:
-    print(g)
-    break
-# print(test)
+# plot
+lines = [go.Scatter(
+    x=g[1]['Year'],
+    y=g[1]['Urban (%)']
+) for g in grouped_df]
+
+fig = go.Figure(data=lines)
+fig.layout.template = 'plotly_dark'
+fig.show()
