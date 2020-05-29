15e27d4bfec7f7bf8ec105f5b3e6f9d18718fc8a
Shervin Azadi <shervinazadi.arch@gmail.com>
2020-05-30 00:04:41 +0200

    layout of the plot

--- VIS/PY_Urbanization/Urbanization.py
+++ VIS/PY_Urbanization/Urbanization.py
@@ -12,12 +12,27 @@ df = df[df['Year'] >= 1950]
 # group by countries
 grouped_df = df.groupby(['Entity'])
 
-# plot
+# plot each group to a line
 lines = [go.Scatter(
+    name=g[0],
+    line_shape='spline',
+    line=dict(width=0.5),
     x=g[1]['Year'],
     y=g[1]['Urban (%)']
 ) for g in grouped_df]
 
+# add lines to figure
 fig = go.Figure(data=lines)
+
+# set the theme
 fig.layout.template = 'plotly_dark'
+
+# add meta data and plot range
+fig.update_layout(title='Urbanization per Countries',
+                  xaxis_title='Year',
+                  yaxis_title='Percentage of Population Living in Urban Areas',
+                  yaxis=dict(range=[0, 100]),
+                  xaxis=dict(range=[1950, 2050]),
+                  )
+
 fig.show()
